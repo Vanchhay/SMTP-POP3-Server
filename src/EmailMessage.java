258c483882c349d8822944148c51043401fa73c4
vanchhay <vanchhay>
2018-07-10 17:49:56 +0700

    Initialization of EmailMessage

---
+++ src/EmailMessage.java
@@ -0,0 +1,37 @@
+public class EmailMessage {
+
+	private String mailFrom, mailTo;
+	private String subject, message;
+
+	public String getMailFrom() {
+		return mailFrom;
+	}
+
+	public void setMailFrom(String mailFrom) {
+		this.mailFrom = mailFrom;
+	}
+
+	public String getMailTo() {
+		return mailTo;
+	}
+
+	public void setMailTo(String mailTo) {
+		this.mailTo = mailTo;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
