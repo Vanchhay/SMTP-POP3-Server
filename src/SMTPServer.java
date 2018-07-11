a5207c0765e76a63528f3b34ab5b801b4527c996
vanchhay <vanchhay>
2018-07-11 23:57:28 +0700

    Added: handle all command (version 1)

--- src/SMTPServer.java
+++ src/SMTPServer.java
@@ -2,11 +2,13 @@ import java.io.*;
 import java.net.*;
 
 class SMTPServer {
+
+	private static EmailMessage email;
+
 	protected final static String CRLF = "\r\n";
 	public static void main(String argv[]) throws Exception {
 		String command;
 		ServerSocket welcomeSocket = new ServerSocket(6782);
-		EmailMessage email;
 
 		while (true) {
 
@@ -21,30 +23,49 @@ class SMTPServer {
 
 			// Spliting command
 			String splitedCommand[] = command.split(":", 2);
-
+			System.out.println(email);
 			switch (splitedCommand[0].trim().toUpperCase()) {
 				case "HELO":
 					outToClient.writeBytes("250 Hello" + CRLF);
 					break;
 				case "MAIL FROM":
 					email = new EmailMessage();
-					if(email.getStatus().isEmpty()){
+					System.out.println(email);
+					if(email.getStatus().isEmpty()) {
 						email.setStatus(email.MAIL_FROM);
 					}
-					String mailFrom = splitedCommand[1].replaceAll("[<,>]", "").trim();
-
+					String mailFrom = extractEmail(splitedCommand[1]);
 					if(isValidEmail(mailFrom)){
 						email.setMailFrom(mailFrom);
-						outToClient.writeBytes("250 " + mailFrom + "Accepted" + CRLF);
+						outToClient.writeBytes("250 " + mailFrom + " Accepted." + CRLF);
 					}else{
-						outToClient.writeBytes("421 unsupported domain!!");
+						outToClient.writeBytes("421 Service not available, closing transmission channel");
 					}
 					break;
 				case "RCPT TO":
-					outToClient.writeBytes("250 OK" + CRLF);
+					System.out.println(email);
+					if(!(email.getStatus().equalsIgnoreCase(email.MAIL_FROM))) {
+						outToClient.writeBytes("500 Syntax error, command unrecognised.");
+					}
+					String mailTo = extractEmail(splitedCommand[1]);
+					if(isValidEmail(mailTo)){
+						email.setMailTo(mailTo);
+						outToClient.writeBytes("250 " + mailTo + " Accepted" + CRLF);
+					}else{
+						outToClient.writeBytes("421 Service not available, closing transmission channel");
+					}
 					break;
 				case "DATA":
-					outToClient.writeBytes("250 Ok message accepted for delivery: queued as 12345" + CRLF);
+					if(!(email.getStatus().equalsIgnoreCase(email.RCPT_TO))) {
+						outToClient.writeBytes("500 Syntax error, command unrecognised.");
+					}
+					outToClient.writeBytes("354 Start mail input; end with <CRLF>.<CRLF>" + CRLF);
+					// After get the message then send
+					if(true){ // if successfully sent
+						outToClient.writeBytes("250 Email has successfully sent." + CRLF);
+					}else{
+						outToClient.writeBytes("552 Transaction Fail" + CRLF);
+					}
 					break;
 				case "QUIT":
 					outToClient.writeBytes("221 Bye" + CRLF);
@@ -72,8 +93,11 @@ class SMTPServer {
 				return true;
 			}
 		}
-		System.out.println("returning false");
 		return false;
+	}
+
+	public static String extractEmail(String unExtractMail){
+		return unExtractMail.replaceAll("[<,>]", "").trim();
 	}
 
 }
