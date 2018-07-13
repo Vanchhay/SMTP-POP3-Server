2988cb7ba86fa295393a791046fc23c95317ce06
vanchhay <vanchhay>
2018-07-13 14:30:02 +0700

    Updated: handle multiple commands before closing the session.

--- src/SMTPServer.java
+++ src/SMTPServer.java
@@ -3,7 +3,7 @@ import java.net.*;
 
 class SMTPServer {
 
-	private static EmailMessage email = null;
+	public static EmailMessage email = null;
 
 	protected final static String CRLF = "\r\n";
 	public static void main(String argv[]) throws Exception {
@@ -14,81 +14,109 @@ class SMTPServer {
 
 			// Get connection
 			Socket connectionSocket = welcomeSocket.accept();
-			System.out.println("received connection");
+			System.out.println("received connection :" + connectionSocket);
 
 			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-			command = inFromClient.readLine();
-			System.out.println("Received: " + command);
+			while(!connectionSocket.isClosed()) {
+				command = inFromClient.readLine();
+				System.out.println("Received: " + command);
 
-			// Spliting command
-			String splitedCommand[] = command.split(":", 2);
-			switch (splitedCommand[0].trim().toUpperCase()) {
-				case "HELO":
-					outToClient.writeBytes("250 Hello" + CRLF);
-					break;
-				case "MAIL FROM":
-					email = new EmailMessage();
-					if(email.getStatus().isEmpty()) {
-						email.setStatus(email.MAIL_FROM);
-					}
-					String mailFrom = extractEmail(splitedCommand[1]);
-					if(isValidEmail(mailFrom)){
-						email.setMailFrom(mailFrom);
-						outToClient.writeBytes("250 <" + mailFrom + "> Accepted." + CRLF);
-					}else{
-						outToClient.writeBytes("421 Service not available, closing transmission channel");
-					}
-					break;
-				case "RCPT TO":
-					if(email == null){
-						outToClient.writeBytes("Email null, MAIL FROM Command is missing." +CRLF);
-						break;
-					}
-					if(!(email.getStatus().equalsIgnoreCase(email.MAIL_FROM))) {
-						outToClient.writeBytes("500 Syntax error, command unrecognised. Did you forget MAIL FROM Command." + CRLF);
-						break;
-					}else{
-						email.setStatus(email.RCPT_TO);
-					}
-					String mailTo = extractEmail(splitedCommand[1]);
-					if(isValidEmail(mailTo)){
-						email.setMailTo(mailTo);
-						outToClient.writeBytes("250 <" + mailTo + "> Accepted" + CRLF);
-					}else{
-						outToClient.writeBytes("421 Service not available, closing transmission channel" +CRLF);
-					}
-					break;
-				case "DATA":
-					if(email == null){
-						outToClient.writeBytes("Email null, MAIL FROM Command is missing." +CRLF);
-						break;
-					}
-					if(!(email.getStatus().equalsIgnoreCase(email.RCPT_TO))) {
-						outToClient.writeBytes("500 Syntax error, command unrecognised." + CRLF);
-						break;
-					}
-					outToClient.writeBytes("354 Start mail input; end with <CRLF>.<CRLF>" + CRLF);
+				// Spliting command
+				String splitedCommand[];
+				if (command.startsWith("HELO")){
+					splitedCommand = command.split(" ", 2);
+				}else{
+					splitedCommand = command.split(":",2);
+				}
 
-					// After get the message then send
-					String data = inFromClient.readLine();
-					if(!data.isEmpty()){ // if successfully sent
-						email.setMessage(data);
-						System.out.println(data);
-						outToClient.writeBytes("250 Email has successfully sent." + CRLF);
-					}else{
-						outToClient.writeBytes("552 Transaction Fail" + CRLF);
-					}
-					break;
-				case "QUIT":
-					System.out.println("QUIT");
-					connectionSocket.close();
-					outToClient.writeBytes("221 Bye" + CRLF);
-					break;
-				default:
-					outToClient.writeBytes("Undefined Command");
-					break;
+				switch (splitedCommand[0].trim().toUpperCase()) {
+					case "HELO":
+						outToClient.writeBytes("250 Hello " +CRLF);
+						break;
+					case "MAIL FROM":
+						System.out.println("mail from");
+						email = new EmailMessage();
+
+						String mailFrom = extractEmail(splitedCommand[1]);
+						if (isValidEmail(mailFrom)) {
+							email.setMailFrom(mailFrom);
+							email.setStatus(email.MAIL_FROM);
+							outToClient.writeBytes("250 <" + mailFrom + "> Accepted." +CRLF);
+						} else {
+							outToClient.writeBytes("421 Service not available, closing transmission channel" +CRLF);
+						}
+						break;
+					case "RCPT TO":
+						System.out.println("rcpt to");
+						if (email == null) {
+							outToClient.writeBytes("Email null, MAIL FROM Command is missing." +CRLF);
+							break;
+						}else{
+							if((!(email.getStatus().equalsIgnoreCase(email.MAIL_FROM))) && !(email.getStatus().equalsIgnoreCase(email.RCPT_TO))){
+								outToClient.writeBytes("500 Syntax error, command unrecognised. Did you forget MAIL FROM Command." +CRLF);
+								break;
+							}
+							if(((email.getStatus().equalsIgnoreCase(email.MAIL_FROM)))){
+								email.setStatus(email.RCPT_TO);
+							}
+							String mailTo = extractEmail(splitedCommand[1]);
+							if (isValidEmail(mailTo)) {
+								email.setMailTo(mailTo);
+								outToClient.writeBytes("250 <" + mailTo + "> Accepted" +CRLF);
+							} else {
+								outToClient.writeBytes("421 Service not available, closing transmission channel." +CRLF);
+							}
+						}
+						break;
+					case "DATA":
+						System.out.println("Data");
+						if (email == null) {
+							outToClient.writeBytes("Email null, MAIL FROM Command is missing." +CRLF);
+							break;
+						}
+						if (!(email.getStatus().equalsIgnoreCase(email.RCPT_TO))) {
+							outToClient.writeBytes("500 Syntax error, command unrecognised." +CRLF);
+							break;
+						}
+						outToClient.writeBytes("354 Start mail input; end with <CRLF>.<CRLF>" + '\n');
+						outToClient.writeBytes("Date: " + '\n');
+						outToClient.writeBytes("From: " +email.getMailFrom() + '\n');
+						outToClient.writeBytes("To: ");
+						for (String rcpt : email.getMailTo()){
+							outToClient.writeBytes(rcpt + " , ");
+						}
+//						if(!email.getMailCc().isEmpty()) {
+//							outToClient.writeBytes("CC: ");
+//							for (String cc : email.getMailCc())
+//								outToClient.writeBytes(cc + " , ");
+//						}
+//						if(!email.getMailBcc().isEmpty()) {
+//							outToClient.writeBytes("BCC: ");
+//							for (String bcc : email.getMailBcc())
+//								outToClient.writeBytes(bcc + " , ");
+//						}
+						outToClient.writeBytes(CRLF);
+
+						// After get the message then send
+						String data = inFromClient.readLine();
+						if (!data.isEmpty()) { // if successfully sent
+							email.setMessage(data);
+							System.out.println(data);
+							outToClient.writeBytes("250 Email has successfully sent." +CRLF);
+						} else {
+							outToClient.writeBytes("552 Transaction Fail." +CRLF);
+						}
+						break;
+					case "QUIT":
+						System.out.println("QUIT");
+						outToClient.writeBytes("221 Bye ");
+						connectionSocket.close();
+						break;
+					default:
+
+				}
 			}
 		}
 	}
