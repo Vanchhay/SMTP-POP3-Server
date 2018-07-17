fb4afc980830d15ec4dc05ba7be5d4c430cb8ddd
vanchhay <vanchhay>
2018-07-17 17:41:42 +0700

    Updated: Add email to H2, CC and BCC not yet done.

--- src/SMTPServer.java
+++ src/SMTPServer.java
@@ -1,9 +1,22 @@
 import java.io.*;
 import java.net.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.h2.expression.Function.CURRENT_TIMESTAMP;
 
 class SMTPServer {
 
-	public static EmailMessage email = null;
+	public static Envelop email = null;
+
+	static final String JDBC_DRIVER = "org.h2.Driver";
+	static final String DB_URL = "jdbc:h2:tcp://localhost/~/test";
+
+	//  Database credentials
+	static final String USER = "admin";
+	static final String PASS = "admin123";
 
 	protected final static String CRLF = "\r\n";
 	public static void main(String argv[]) throws Exception {
@@ -20,7 +33,7 @@ class SMTPServer {
 			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
 			if(connectionSocket.isConnected()){
-				outToClient.writeBytes("250" +CRLF);
+				outToClient.writeBytes("220" +CRLF);
 			}
 
 			while(!connectionSocket.isClosed()) {
@@ -29,7 +42,7 @@ class SMTPServer {
 
 				// Spliting command
 				String splitedCommand[];
-				if (command.startsWith("HELO")) {
+				if (command.startsWith("HELO") || command.startsWith("EHLO")) {
 					splitedCommand = command.split(" ",2);
 				} else {
 					splitedCommand = command.split(":",2);
@@ -38,9 +51,12 @@ class SMTPServer {
 					case "HELO":
 						outToClient.writeBytes("250 Hello " +CRLF);
 						break;
+					case "EHLO":
+						outToClient.writeBytes("250 Hello " +CRLF);
+						break;
 					case "MAIL FROM":
 						System.out.println("mail from");
-						email = new EmailMessage();
+						email = new Envelop();
 
 						String mailFrom = extractEmail(splitedCommand[1]);
 						if (isValidEmail(mailFrom)) {
@@ -83,36 +99,50 @@ class SMTPServer {
 							outToClient.writeBytes("500 Syntax error, command unrecognised." +CRLF);
 							break;
 						}
-						outToClient.writeBytes("354 Start mail input; end with <CRLF>.<CRLF>" + '\n');
-						outToClient.writeBytes("Date: " + '\n');
-						outToClient.writeBytes("From: " +email.getMailFrom() + '\n');
-						outToClient.writeBytes("To: ");
-						for (String rcpt : email.getMailTo()){
-							outToClient.writeBytes(rcpt + " , ");
-						}
-//						if(!email.getMailCc().isEmpty()) {
-//							outToClient.writeBytes("CC: ");
-//							for (String cc : email.getMailCc())
-//								outToClient.writeBytes(cc + " , ");
-//						}
-//						if(!email.getMailBcc().isEmpty()) {
-//							outToClient.writeBytes("BCC: ");
-//							for (String bcc : email.getMailBcc())
-//								outToClient.writeBytes(bcc + " , ");
-//						}
-						outToClient.writeBytes(CRLF);
+						outToClient.writeBytes("354 Start mail input; end with <CRLF>.<CRLF>" +CRLF);
 
 						// After get the message then send
-						String data = inFromClient.readLine();
-						if (!data.isEmpty()) { // if successfully sent
-							email.setMessage(data);
-							System.out.println(data);
-							outToClient.writeBytes("250 Email has successfully sent." +CRLF);
-						} else {
-							outToClient.writeBytes("552 Transaction Fail." +CRLF);
+						String data;
+						boolean success;
+						boolean startMessage = false;
+						while(true) {
+							email.setStatus(email.DATA);
+							data = inFromClient.readLine().trim();
+							if(data.startsWith("Subject:")){
+								String subject = data.substring(8).trim();
+								email.setSubject(subject);
+							}
+							if(data.trim().startsWith(".")){
+								if(addToH2(email)) {
+									email.setStatus(email.SENT);
+									outToClient.writeBytes("250 Email has successfully sent." + CRLF);
+									success = true;
+									break;
+								}
+							}
+							if(!data.isEmpty()){ // if successfully sent
+								if(startMessage){
+									email.setMessage(email.getMessage().concat(data + '\n'));
+								}
+							}
+							if(data.startsWith("Content-Transfer-Encoding")){
+								startMessage = true;
+							}
 						}
-						break;
+						if(success) {
+							break;
+						}else{
+							outToClient.writeBytes("552 Transaction Fail." + CRLF);
+							break;
+						}
 					case "QUIT":
+						System.out.println("==============================");
+						System.out.println("STATUS: "+ email.getStatus());
+						System.out.println("FROM: "+ email.getMailFrom());
+						System.out.println("TO: "+ email.getMailTo());
+						System.out.println("SUBJECT: "+ email.getSubject());
+						System.out.println("MESSAGE: "+ email.getMessage());
+						System.out.println("==============================");
 						System.out.println("QUIT");
 						outToClient.writeBytes("221 Bye ");
 						connectionSocket.close();
@@ -121,7 +151,6 @@ class SMTPServer {
 						System.out.println("Default");
 						outToClient.writeBytes("Please try again! "+CRLF);
 						break;
-
 				}
 			}
 		}
@@ -147,4 +176,53 @@ class SMTPServer {
 		return unExtractMail.replaceAll("[<,>]", "").trim();
 	}
 
+	public static boolean addToH2(Envelop email){
+		Connection conn = null;
+		Statement stmt = null;
+
+		try{
+			String mailTo = "";
+
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// Extracting MailTo list
+			for (String to : email.getMailTo()) {
+				mailTo = mailTo.concat(to + ",");
+			}
+			// Execute a query
+			stmt = conn.createStatement();
+			String sql = "insert into mail values(null, "+
+					"'"+email.getSubject()+"'," +
+					"'"+email.getMessage()+"', " +
+					"'"+email.getMailFrom()+"', " +
+					"'"+mailTo+"', " +
+					"'cc@gmail.com,cc1@gmail.com', " +
+					"'bcc@gmail.com,bcc1@gmail.com', " +
+					"current_timestamp()" +
+					")";
+
+			stmt.executeUpdate(sql);
+
+			stmt.close();
+			conn.close();
+		} catch(SQLException se) {
+			se.printStackTrace();
+			return false;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			try {
+				if(stmt!=null) stmt.close();
+			} catch(SQLException se2) {
+			}
+			try {
+				if(conn!=null) conn.close();
+			} catch(SQLException se) {
+				se.printStackTrace();
+			}
+		}
+		return true;
+	}
 }
