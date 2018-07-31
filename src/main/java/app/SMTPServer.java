7f9ace5ca368ca5a1f8ea1ae96bf4100e2f3eb43
vanchhay <vanchhay>
2018-07-31 13:50:34 +0700

    Updated: Move all classes to app package.

--- src/main/java/app/SMTPServer.java
+++ src/main/java/app/SMTPServer.java
@@ -1,4 +1,4 @@
-package main.java;
+package app;
 
 import java.io.*;
 import java.net.*;
--- src/main/java/app/SMTPThread.java
+++ src/main/java/app/SMTPThread.java
@@ -1,4 +1,4 @@
-package main.java;
+package app;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
